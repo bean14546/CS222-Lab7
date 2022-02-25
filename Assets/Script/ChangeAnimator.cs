e18dc8e3c77e68a93b510aaff8f93c5305886270
bean1888@outlook.com <bean1888@outlook.com>
2022-02-25 18:38:05 +0700

    Lab7

---
+++ Assets/Script/ChangeAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    public void getAnimatorDefalt(){
+        this.GetComponent<Animator>().Play("Dancing");
+    }
+
+    public void getAnimatorWalk(){
+        this.GetComponent<Animator>().Play("Walking");
+    }
+}
